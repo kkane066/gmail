4e95b6a8fc92e7981c7d5247814f0298a2876f54
kkane066 <kkane066@uottawa.ca>
2022-02-20 23:31:19 -0500

    Day 4
    Working on emailList. Running into issues with SL and SR sitting at opposite ends of the container

--- src/app/EmailList.js
+++ src/app/EmailList.js
@@ -2,11 +2,14 @@
 
 import React from "react";
 import "./EmailList.css";
-import { Checkbox } from "@mui/material";
-import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import { Checkbox, IconButton } from "@mui/material";
 import ArrowDropDown from "@mui/icons-material/ArrowDropDown";
 import RefreshIcon from "@mui/icons-material/Refresh";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import KeyboardIcon from "@mui/icons-material/Keyboard";
+import SettingsIcon from "@mui/icons-material/Settings";
 
 function EmailList() {
   return (
@@ -14,9 +17,29 @@ function EmailList() {
       <div className="emailList__settings">
         <div className="emailList__settingsLeft">
           <Checkbox />
-          <ArrowDropDown />
-          <RefreshIcon />
-          <MoreVertIcon />
+          <IconButton>
+            <ArrowDropDown />
+          </IconButton>
+          <IconButton>
+            <RefreshIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+        <div className="emailList__settingsRight">
+          <IconButton>
+            <ChevronLeftIcon />
+          </IconButton>
+          <IconButton>
+            <ChevronRightIcon />
+          </IconButton>
+          <IconButton>
+            <KeyboardIcon />
+          </IconButton>
+          <IconButton>
+            <SettingsIcon />
+          </IconButton>
         </div>
       </div>
     </div>
