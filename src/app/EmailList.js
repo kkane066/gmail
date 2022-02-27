46aa320e9765578663e3b105eb2a87ac9b77168c
kkane066 <kkane066@uottawa.ca>
2022-02-27 00:30:10 -0500

    Day 5
    Got the setting elements to justify. Set the widths.

--- src/app/EmailList.js
+++ src/app/EmailList.js
@@ -10,12 +10,16 @@ import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
 import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 import KeyboardIcon from "@mui/icons-material/Keyboard";
 import SettingsIcon from "@mui/icons-material/Settings";
+import Section from "./Section";
+import InboxIcon from "@mui/icons-material/Inbox";
+import PeopleIcon from "@mui/icons-material/People";
+import LocalOfferIcon from "@mui/icons-material/LocalOffer";
 
 function EmailList() {
   return (
     <div className="emailList">
-      <div className="emailList__settings">
-        <div className="emailList__settingsLeft">
+      <div className="emailList__setting">
+        <div className="emailList__settingLeft">
           <Checkbox />
           <IconButton>
             <ArrowDropDown />
@@ -27,7 +31,8 @@ function EmailList() {
             <MoreVertIcon />
           </IconButton>
         </div>
-        <div className="emailList__settingsRight">
+
+        <div className="emailList__settingRight">
           <IconButton>
             <ChevronLeftIcon />
           </IconButton>
@@ -41,6 +46,16 @@ function EmailList() {
             <SettingsIcon />
           </IconButton>
         </div>
+      </div>
+      <div className="emailList__section">
+        <Section Icon={InboxIcon} title="Primary" color="red" selected />
+        <Section Icon={PeopleIcon} title="Social" color="blue" selected />
+        <Section
+          Icon={LocalOfferIcon}
+          title="Promotions"
+          color="green"
+          selected
+        />
       </div>
     </div>
   );
